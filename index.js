e70a6e42e98a52ecb800e83eca8c84d4dc4b0a16
Beidou Zhang <beidouz@hotmail.com>
2018-08-26 15:21:16 -0400

    responsive

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const collectionID = 1120118;
 const numImagesAvailable = 40;
 
-for (var i = 0; i < 25; i++) {
-  var imageWidth = 200;
-  var imageHeight = 300;
+function getRandomNumber(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+for (var i = 0; i < numImagesAvailable; i++) {
+  var imageWidth = getRandomNumber(200, 400);
+  var imageHeight =  getRandomNumber(200, 400);
   var randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
   fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomImageIndex}`) 
   .then((response)=> { 
     var newImage = '<img src="'+`${response.url}`+'" alt="gallery image"/>'; 
     document.getElementById("image-container").innerHTML += newImage;
-    // console.log(newImage);
   })
 }
-
-
-
