0e361f4b599d42d27ea25292965a09193beacce1
Beidou Zhang <beidouz@hotmail.com>
2018-08-26 12:21:54 -0400

    pic size?

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const collectionID = 1120118;
+const numImagesAvailable = 40;
+
 for (var i = 0; i < 25; i++) {
-  var randomImageIndex = Math.floor(Math.random() * 40);
-  fetch(`https://source.unsplash.com/collection/1120118/?sig=${randomImageIndex}`) 
+  var imageWidth = 200;
+  var imageHeight = 300;
+  var randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+  fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomImageIndex}`) 
   .then((response)=> { 
     var newImage = '<img src="'+`${response.url}`+'" alt="gallery image"/>'; 
     document.getElementById("image-container").innerHTML += newImage;
